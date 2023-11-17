2662dceb460ac5c3cc74c0a6ee29c546b55fa8c6
gabrielthirai <gabrielt.hirai@gmail.com>
2023-11-17 14:11:43 -0300

    modificando os botão de enviar

--- src/components/pages/Produtos.js
+++ src/components/pages/Produtos.js
@@ -18,31 +18,37 @@ function Produtos(){
                 <div className={styles.card}>
                     <img src={cs} alt='Logo CS'></img>
                     <h1>Counter Strike 2</h1>
+                    <p>R$ 39,99</p>
                     <Link to="/carrinho" >Comprar</Link>
                 </div>    
                 <div className={styles.card}>
                     <img src={fc} alt='Logo FC 24'></img>
                     <h1>FC 24</h1>
+                    <p>R$ 99,99</p>
                     <Link to="/carrinho" >Comprar</Link>
                 </div> 
                 <div className={styles.card}>
                     <img src={gta} alt='Logo GTA'></img>
                     <h1>Grand Theft Auto V</h1>
+                    <p>R$ 49,99</p>
                     <Link to="/carrinho" >Comprar</Link>
                 </div> 
                 <div className={styles.card}>
                     <img src={minecraft} alt='Logo Minecraft'></img>
                     <h1>Minecraft</h1>
+                    <p>R$ 9,99</p>
                     <Link to="/carrinho" >Comprar</Link>
                 </div> 
                 <div className={styles.card}>
                     <img src={spider} alt='Logo Spider Man'></img>
                     <h1>Spider Man 2</h1>
+                    <p>R$ 199,99</p>
                     <Link to="/carrinho" >Comprar</Link>
                 </div> 
                 <div className={styles.card}>
                     <img src={zelda} alt='Logo Zelda'></img>
                     <h1>Zelda: Tears of the Kingdom</h1>
+                    <p>R$ 59,99</p>
                     <Link to="/carrinho" >Comprar</Link>
                 </div> 
                 
